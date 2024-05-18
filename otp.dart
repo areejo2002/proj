7dfa5e563f1b82144e3eb70dad1f0c89a8853289
areejo2002 <153197642+areejo2002@users.noreply.github.com>
2024-05-18 20:06:42 +0300

    Update otp.dart

--- otp.dart
+++ otp.dart
@@ -1,6 +1,47 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'newpass.dart';
-class otp extends StatelessWidget {
+import 'second_page.dart';
+import 'profile.dart';
+import 'favourite.dart';
+import 'navbar.dart';
+
+class otp extends StatefulWidget {
+  @override
+  _OtpPageState createState() => _OtpPageState();
+}
+
+class _OtpPageState extends State<otp> {
+  int _selectedIndex = 0;
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+      if (index == 1) {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => favourite(),
+          ),
+        );
+      } else if (index == 2) {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => profile(),
+          ),
+        );
+      } else if (index == 0) {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => secondPage(),
+          ),
+        );
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -9,6 +50,7 @@ class otp extends StatelessWidget {
       ),
       body: Stack(
         children: [
+          // Background image
           Container(
             width: 530,
             height: 900,
@@ -20,6 +62,7 @@ class otp extends StatelessWidget {
               ),
             ),
           ),
+          // App title
           Positioned(
             left: 100,
             top: 20,
@@ -34,6 +77,7 @@ class otp extends StatelessWidget {
               ),
             ),
           ),
+          // Verification code label
           Positioned(
             left: 30,
             top: 180,
@@ -48,93 +92,46 @@ class otp extends StatelessWidget {
               ),
             ),
           ),
+          // OTP input fields
           Positioned(
             left: 30,
             top: 250,
             child: Row(
               children: [
-                Container(
-                  width: 70,
-                  height: 40,
-                  child: TextField(
-                    decoration: InputDecoration(
-                      filled: true,
-                      fillColor: Colors.grey[200],
-                      hintText: '',
-                      hintStyle: TextStyle(
-                        color: Colors.grey,
+                for (int i = 0; i < 4; i++)
+                  Container(
+                    width: 70,
+                    height: 40,
+                    margin: EdgeInsets.only(right: 10), // Add margin between input fields
+                    child: TextField(
+                      textAlign: TextAlign.center, // Center align the text
+                      keyboardType: TextInputType.number,
+                      inputFormatters: <TextInputFormatter>[
+                        FilteringTextInputFormatter.digitsOnly,
+                        LengthLimitingTextInputFormatter(1), // Allow only one digit per box
+                      ],
+                      decoration: InputDecoration(
+                        filled: true,
+                        fillColor: Colors.grey[200],
+                        hintText: '',
+                        hintStyle: TextStyle(
+                          color: Colors.grey,
+                          fontFamily: 'Tajawal',
+                        ),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10.0),
+                        ),
+                      ),
+                      style: TextStyle(
+                        color: Colors.black,
                         fontFamily: 'Tajawal',
                       ),
                     ),
-                    style: TextStyle(
-                      color: Colors.black,
-                      fontFamily: 'Tajawal',
-                    ),
                   ),
-                ),
-                SizedBox(width: 10),
-                Container(
-                  width: 70,
-                  height: 40,
-                  child: TextField(
-                    decoration: InputDecoration(
-                      filled: true,
-                      fillColor: Colors.grey[200],
-                      hintText: '',
-                      hintStyle: TextStyle(
-                        color: Colors.grey,
-                        fontFamily: 'Tajawal',
-                      ),
-                    ),
-                    style: TextStyle(
-                      color: Colors.black,
-                      fontFamily: 'Tajawal',
-                    ),
-                  ),
-                ),
-                SizedBox(width: 10),
-                Container(
-                  width: 70,
-                  height: 40,
-                  child: TextField(
-                    decoration: InputDecoration(
-                      filled: true,
-                      fillColor: Colors.grey[200],
-                      hintText: '',
-                      hintStyle: TextStyle(
-                        color: Colors.grey,
-                        fontFamily: 'Tajawal',
-                      ),
-                    ),
-                    style: TextStyle(
-                      color: Colors.black,
-                      fontFamily: 'Tajawal',
-                    ),
-                  ),
-                ),
-                SizedBox(width: 10),
-                Container(
-                  width: 70,
-                  height: 40,
-                  child: TextField(
-                    decoration: InputDecoration(
-                      filled: true,
-                      fillColor: Colors.grey[200],
-                      hintText: '',
-                      hintStyle: TextStyle(
-                        color: Colors.grey,
-                        fontFamily: 'Tajawal',
-                      ),
-                    ),
-                    style: TextStyle(
-                      color: Colors.black,
-                      fontFamily: 'Tajawal',
-                    ),
-                  ),
-                ),
               ],
             ),
           ),
+          // Resend code option
           Positioned(
             left: 230,
             top: 295,
@@ -144,13 +141,13 @@ class otp extends StatelessWidget {
                 color: Color(0xFF156260),
                 fontSize: 14,
                 fontFamily: 'Tajawal',
-                fontWeight: FontWeight.w300 ,
+                fontWeight: FontWeight.w300,
                 decoration: TextDecoration.underline,
-
                 height: 0,
               ),
             ),
           ),
+          // Verify button
           Positioned(
             left: 95,
             top: 380,
@@ -159,10 +156,10 @@ class otp extends StatelessWidget {
                 Navigator.push(
                   context,
                   MaterialPageRoute(builder: (context) => newpass()),
-                ); // يمكنك إضافة الإجراء المطلوب هنا
+                );
               },
               child: Container(
-                width: 150, // تعيين عرض الزر إلى 200 وحدة
+                width: 150,
                 child: Center(
                   child: Text(
                     'تحقق',
@@ -185,6 +182,10 @@ class otp extends StatelessWidget {
             ),
           ),
         ],
+      ),
+      bottomNavigationBar: CustomNavigationBar(
+        selectedIndex: _selectedIndex,
+        onItemTapped: _onItemTapped,
       ),
     );
   }
